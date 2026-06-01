fc6eadb619ee4de6f0d0ab5031440895e55283d3
LesterCerioli <lesterlucasit@hotmail.com>
2026-06-01 10:36:18 -0400

    feat(#41): Building entities for DeFi module

--- app/services/defi/domain/entities/__init__.py
+++ app/services/defi/domain/entities/__init__.py
@@ -1,6 +1,22 @@
+from .market_index import MarketIndex
 from .pool import Pool
 from .position import Position
 from .protocol import Protocol, ProtocolName
+from .quote import Quote
+from .research_report import ResearchReport
 from .token import Token
+from .unsigned_transaction import UnsignedTransaction
+from .wallet_session import WalletSession
 
-__all__ = ["Pool", "Position", "Protocol", "ProtocolName", "Token"]
+__all__ = [
+    "MarketIndex",
+    "Pool",
+    "Position",
+    "Protocol",
+    "ProtocolName",
+    "Quote",
+    "ResearchReport",
+    "Token",
+    "UnsignedTransaction",
+    "WalletSession",
+]
